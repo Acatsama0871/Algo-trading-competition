bf3901a32eeca828e76946ca9eda69dcdc5c9238
Acatsama0871 <51130578+Acatsama0871@users.noreply.github.com>
2020-03-11 23:01:47 -0400

    Update run.py
    Delete the test functions

--- First competition/run.py
+++ First competition/run.py
@@ -6,53 +6,6 @@ import shift
 import datetime as dt
 
 
-def show(trader: shift.Trader):
-    """
-    This method provides information on the structure of PortfolioSummary and PortfolioItem objects:
-     get_portfolio_summary() returns a PortfolioSummary object with the following data:
-     1. Total Buying Power (get_total_bp())
-     2. Total Shares (get_total_shares())
-     3. Total Realized Profit/Loss (get_total_realized_pl())
-     4. Timestamp of Last Update (get_timestamp())
-
-     get_portfolio_items() returns a dictionary with "symbol" as keys and PortfolioItem as values,
-     with each providing the following information:
-     1. Symbol (get_symbol())
-     2. Shares (get_shares())
-     3. Price (get_price())
-     4. Realized Profit/Loss (get_realized_pl())
-     5. Timestamp of Last Update (get_timestamp())
-    :param trader:
-    :return:
-    """
-
-    print("Buying Power\tTotal Shares\tTotal P&L\tTimestamp")
-    print(
-        "%12.2f\t%12d\t%9.2f\t%26s"
-        % (
-            trader.get_portfolio_summary().get_total_bp(),
-            trader.get_portfolio_summary().get_total_shares(),
-            trader.get_portfolio_summary().get_total_realized_pl(),
-            trader.get_portfolio_summary().get_timestamp(),
-        )
-    )
-
-    print()
-
-    print("Symbol\t\tShares\t\tPrice\t\t  P&L\tTimestamp")
-    for item in trader.get_portfolio_items().values():
-        print(
-            "%6s\t\t%6d\t%9.2f\t%9.2f\t%26s"
-            % (
-                item.get_symbol(),
-                item.get_shares(),
-                item.get_price(),
-                item.get_realized_pl(),
-                item.get_timestamp(),
-            )
-        )
-
-    return
 
 def main(argv):
     # connect to shift
@@ -69,7 +22,7 @@ def main(argv):
     time.sleep(10)
 
     # Set stop time
-    stop_time = dt.time(9, 51, 00) # todo delete
+    stop_time = dt.time(15, 30, 33) # todo delete
 
     # Buy at the first time
     while True:
@@ -109,9 +62,6 @@ def main(argv):
         if current_time > stop_time:
             break
 
-
-    # Show
-    show(trader) # todo delete
 
     # disconnect
     trader.disconnect()
